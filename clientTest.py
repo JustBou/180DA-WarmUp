4c475a129e6a3f759c418bb95c7bccd35c285dd6
justBou <bou22716@gmail.com>
2024-10-19 17:26:57 -0700

    clientTest upload

---
+++ clientTest.py
@@ -0,0 +1,10 @@
+import socket
+
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.68.51', 8080))
+client.sendall('I am Client\n'.encode())
+from_server = client.recv(4096)
+client.close()
+print(from_server)
