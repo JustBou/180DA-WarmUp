f781f79e16e0af6582e00297cb8df1ebb28f42f7
justBou <bou22716@gmail.com>
2024-10-23 10:51:22 -0700

    week 3 files; changed clientTest for school IP and simple classifer for IMU

--- clientTest.py
+++ clientTest.py
@@ -3,7 +3,7 @@ import socket
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.68.51', 8080))
+client.connect(('172.26.189.204', 8080))
 client.sendall('I am Client\n'.encode())
 from_server = client.recv(4096)
 client.close()
